9a0f418585b7948fd346d1f2dca2632431794077
Vinicius <vinniimiranda@gmail.com>
2020-07-24 00:22:23 -0300

    Profile

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,7 +1,162 @@
 import React from 'react';
+import { Box, Grid, FormControl, InputLabel, Input, FormControlLabel, Checkbox, useTheme } from '@material-ui/core';
 
 const Profile: React.FC = () => {
-  return <h1>Profile</h1>
+  const theme = useTheme()
+  return <Box display="flex" flexDirection="column">
+    <Box>
+      <h1 style={{
+        color: theme.palette.primary.main
+      }}>Currículo</h1>
+    </Box>
+    <Box display="flex" flexDirection="column">
+      <h2 style={{
+        color: theme.palette.primary.main
+      }}>Dados Pessoais</h2>
+      <Grid container spacing={2}>
+        <Grid item lg={3} md={4} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="name">Nome</InputLabel>
+            <Input
+
+              id="name"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={3} md={4} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="emal">E-mail</InputLabel>
+            <Input
+
+              id="emal"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={2} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">CPF</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={2} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">RG</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={3} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">Data de Nascimento</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box display="flex" flexDirection="column" marginTop="3rem">
+      <h2 style={{
+        color: theme.palette.primary.main
+      }}>Contato e Endereço</h2>
+      <Grid container spacing={2}>
+        <Grid item lg={2} md={3} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="name">Logradouro</InputLabel>
+            <Input
+
+              id="name"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={1} md={1} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="emal">Número</InputLabel>
+            <Input
+
+              id="emal"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={3} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">Bairro</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={3} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">Cidade</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={1} md={1} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">UF</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={2} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">Celular</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+        <Grid item lg={2} md={2} xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="document">Telefone</InputLabel>
+            <Input
+
+              id="document"
+              autoComplete="off"
+            />
+          </FormControl>
+        </Grid>
+      </Grid>
+    </Box>
+    <Box display="flex" flexDirection="column" marginTop="3rem">
+      <h2 style={{
+        color: theme.palette.primary.main
+      }}>Skills</h2>
+      <Grid container spacing={2}>
+        <Grid item>
+          <FormControlLabel
+            control={<Checkbox color={"primary"} checked name="rember" />}
+            label="Javascript"
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  </Box>
 }
 
 export default Profile;
