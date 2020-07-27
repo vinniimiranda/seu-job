30f513c349fdd4a5bab0207fc5468cda6338d1a6
Vinicius <vinniimiranda@gmail.com>
2020-07-27 15:36:01 -0300

    Heroku api added

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 export const API = Axios.create({
-  baseURL: 'http://192.168.0.104:3333',
+  baseURL: process.env.NODE_ENV === 'production' ? 'https://seu-job-api.herokuapp.com' : 'http://192.168.0.104:3333',
   headers: {
     "Content-Type": "application/json",
   },
