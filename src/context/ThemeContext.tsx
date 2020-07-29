3540b11cbcb0776fcc677535f9ddf6219d88bfc4
Vinicius <vinniimiranda@gmail.com>
2020-07-29 13:55:53 -0300

    update favicon

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -18,13 +18,12 @@ const ThemeProvider: React.FC = ({ children }) => {
 
   function toggleTheme () {
     setDarkTheme(prevDarkTheme => {
+      const favicon = window.document.querySelector('#favicon') as HTMLLinkElement
       if (prevDarkTheme) {
-        // @ts-ignore
-        window.document.getElementById('favicon').href = 'favicon_light.png'
+        favicon.setAttribute('href', 'favicon_light.png')
         localStorage.setItem('lightMode', 'true')
       } else {
-        // @ts-ignore
-        window.document.getElementById('favicon').href = 'favicon.png'
+        favicon.setAttribute('href', 'favicon.png')
         localStorage.removeItem('lightMode')
       }
       return !prevDarkTheme
